d60ae2cc43334be274d910d087481a5feb5023e7
Weisberg <stevenweisberg@ufl.edu>
2020-02-10 16:15:47 -0500

    some cleaning; added Gender and Encoding tours to be recorded

--- dsp_dataExtractor.py
+++ dsp_dataExtractor.py
@@ -1,24 +1,40 @@
+## Written by Alex Boone
+## Modified by Steven Weisberg
+## Last edit: 2/10/2020
 
-####
-# ADD the extra line to the stop of each file.
-#THIS IS THE ACTUAL FINAL
-####
+### Purpose of this script is to process location data from the Dual Solution
+### Paradigm, built in Unity 3D. It will output a set of images that can be 
+### hand-coded to determine A) Success, and B) which path was taken (familiar
+### route or novel shortcut). 
 
-Plotter = True
-Time_Dist_Success = False
-
-#******************************************************************************************************
-#                                         PLOTTER
-#******************************************************************************************************
 
 from matplotlib import pyplot
 from matplotlib.backends.backend_pdf import PdfPages
 from imageio import imread
 import os
 
+Plotter = False
+Time_Dist_Success = True
+
+#******************************************************************************************************
+#                                         PLOTTER
+#******************************************************************************************************
+
+
+scriptDir = os.getcwd()
+
+# Expects that the data is one file up from the analysis script
+os.chdir(os.path.dirname(os.getcwd()))
+masterDir = os.getcwd()
+
+
+indir = os.path.join(masterDir,'DSP_RawData')
+outdir = os.path.join(masterDir,'DSP_ProcessedData')
+
 
 def Graph(trialID):
     pyplot.figure(dpi = 350)
+    figureFiletype = ".png"
     pyplot.plot(PosX_array, PosY_array, "k", label = "__nolegend__")
 
     v = [0, 222, 0, 222]
@@ -27,13 +43,17 @@ def Graph(trialID):
     pyplot.xlabel("X")
     pyplot.title(title)
     pyplot.legend(["Path"], loc = 'center left', bbox_to_anchor = (1.0, 0.5))
-
-
+   
+    
+    # Which version of the experiment are we in?
     if (Alt_Exp == "DSPType: 2"):
-        bestImage = "C:\\Users\\stevenweisberg\\Documents\\DSP\\DSP_dataScripts\\Nav_stratAbility_Maps\\DSP2_" + trialID + ".png"
+        expPrefix = "DSP2_"
     else:
-        bestImage = "C:\\Users\\stevenweisberg\\Documents\\DSP\\DSP_dataScripts\\Nav_stratAbility_Maps\\DSP1_" + trialID + ".png"
+        expPrefix = "DSP1_"
 
+    # Open the image with that trial structure
+    imageFilename = expPrefix + trialID + figureFiletype
+    bestImage = os.path.join(scriptDir,'Nav_stratAbility_Maps',imageFilename)
     img = imread(bestImage)
 
 
@@ -41,19 +61,13 @@ def Graph(trialID):
 
     pyplot.imshow(img,zorder=0,extent=[0.0, 222.0, 0.0, 222.0]) #left right bottom top
 
-    '''
-    c_Sx, c_Sy, c_Lx, c_Ly = Graph_shortcut()
-    pyplot.plot(c_Sx, c_Sx, "m", label = "shortcut")
-    pyplot.plot(c_Lx, c_Ly, "g", label = "learned")
-    '''
 
     pp.savefig()
     pyplot.clf()
 
 #######
 if (Plotter == True):
-    indir = 'C:\\Users\\stevenweisberg\\Documents\\DSP_Participants\\'
-    outdir = 'C:\\Users\\stevenweisberg\\Documents\\DSP\\'
+    
 
     currTrial = ""
     Alt_Exp = ""
@@ -62,28 +76,20 @@ if (Plotter == True):
 
     for f in os.listdir(indir):
         #the first file in each directory was this for some reason
-        if (f != '.DS_Store'):
+        if ("txt" in f):
             print (f)
             numOfLines = 0
             Trial_Count = 0
 
-            with open(indir + f) as infile:
-                pp = PdfPages(outdir + f + ".pdf")
+            with open(os.path.join(indir,f)) as infile:
+                filename = f + ".pdf"
+                pp = PdfPages(os.path.join(outdir,filename))
                 PosX_array = []
                 PosY_array = []
 
                 for current_line in infile:
 
-                    #skips the first 7 lines of every new file
-                    ### AMANDA AND STEVE -> CHANGE THIS TO DETECT THE FIRST TRIAL!
-                    if (numOfLines <= 8):
-                        if (numOfLines == 5):
-                            Alt_Exp = current_line[0:10]
-                            print(Alt_Exp)
-                        numOfLines += 1
-
-                    #Checks to see if there's a new trial
-                    elif (current_line.startswith("!!")):
+                    if (current_line.startswith("!!")):
                         if (Trial_Count > 0):
                             Graph(currTrial)
                             PosX_array = []
@@ -172,9 +178,11 @@ def checkFirstMove(line1, line2):
 def appendInput():
     line = []
     line.append(ParticipantNo)
+    line.append(ParticipantGen)
     line.append(Stressor)
     line.append(ExpType)
     line.append(DSPType)
+    line.append(EncodingTours)
     line.append(TrialNo)
     line.append(TrialID)
     line.append(Time_Elapsed)
@@ -184,15 +192,14 @@ def appendInput():
     return line
 
 
+
 if (Time_Dist_Success == True):
 
-    indir = 'C:\\Users\\stevenweisberg\\Documents\\DSP_Participants\\'
-    outdir = 'C:\\Users\\stevenweisberg\\Documents\\DSP\\'
+    
+    outPutHeader = ['ParticipantNo', 'ParticipantGen', 'Stressor','ExpType','DSPType', 'EncodingTours', 'TrialNo', 'TrialID', 'Time Elapsed', 'Distance',
+                     'Status', 'Time_to_First_Movement']
 
-    outPutHeader = ['Participant No', 'Stressor','ExpType','DSPType', 'TrialNo', 'TrialID', 'Time Elapsed', 'Distance',
-                     'Status', 'Time to First Movement']
-
-    wb = xlsxwriter.Workbook("C:\\Users\\stevenweisberg\\Documents\\DSP\\Master_DSP_all.xlsx")
+    wb = xlsxwriter.Workbook(os.path.join(outdir,"Master_DSP_all.xlsx"))
 
     sheet = wb.add_worksheet('Sheet')
 
@@ -204,20 +211,25 @@ if (Time_Dist_Success == True):
     input_line = []
 
     ParticipantNo = ""
-    Stressor =""
-    ExoType = ""
+    Stressor = ""
+    ExpType = ""
     DSPType = ""
+    EncodingTours = ""
+    ParticipantGen = ""
     TrialNo = 0
     TrialID = ""
     Time_Elapsed = 0.0
     Dist = 0.0
     Status = ""
     Time_First = 0.0
-
+    
+    headerCounter = 0
+    trialStart = False
+    
     row = 0
 
     for f in os.listdir(indir):
-        if (f != '.DS_Store'):
+        if ("txt" in f):
             print (f)
             numOfLines = 0
             TrialNo = 0
@@ -230,22 +242,32 @@ if (Time_Dist_Success == True):
             y1 = 0.0
             x2 = 0.0
             y2 = 0.0
-            with open(indir + f) as infile:
-                for current_line in infile: #skips first 7 lines
-                    if (numOfLines <= 7):
+            with open(os.path.join(indir,f)) as infile:
+                for current_line in infile: 
+                    
+                    if headerCounter != 6:
+
                         if current_line.startswith('ParticipantNo'):
                             ParticipantNo = current_line[15:18]
-                        if current_line.startswith('Stressor'):
+                            headerCounter += 1
+                        elif current_line.startswith('Stressor'):
                             Stressor = current_line[10:12]
-                        if current_line.startswith('Exp Type'):
+                            headerCounter += 1                                          
+                        elif current_line.startswith('Exp Type'):
                             ExpType = current_line[11:18]
-                        if current_line.startswith('DSPType'):
+                            headerCounter += 1
+                        elif current_line.startswith('DSPType'):
                             DSPType = current_line[9:10]
-                        numOfLines += 1
-                    elif current_line == '\n':
-                        print('uh oh')
+                            headerCounter += 1
+                        elif current_line.startswith('ParticipantGen'):
+                            ParticipantGen = current_line[16:17]
+                            headerCounter += 1
+                        elif current_line.startswith('Encoding'):
+                            EncodingTours = current_line[16:17]
+                            headerCounter += 1
 
                     elif (current_line.startswith('!!')): #At every new trial, gets info and prints
+                        trialStart = True
                         if TrialNo > 0:
                             info = getInfo(prev_line)
                             Time_Elapsed = float(info[0])
@@ -256,25 +278,24 @@ if (Time_Dist_Success == True):
                             y1 = 0.0
                             x2 = 0.0
                             y2 = 0.0
-
+    
                             input_line = appendInput()
-
-                            #print(input_line)
-
+    
+    
                         TrialID = getTrialID(current_line)
                         TrialID = TrialID[0:7]
-
+    
                         col = 0
-
+    
                         for i in input_line: #Writes to excel file
                             sheet.write(row, col, i)
                             col += 1
-
+    
                         Dist = 0.0
                         row += 1
                         TrialNo += 1
-
-                    else: #Finds time of first movement and sums x and y values for distance
+    
+                    elif trialStart: #Finds time of first movement and sums x and y values for distance
                         if (prev_line.startswith("!")) == False and (prev_line != ""):
                             line1 = getInfo(current_line)
                             line2 = getInfo(prev_line)
@@ -282,44 +303,39 @@ if (Time_Dist_Success == True):
                             y1 = float(line1[2])
                             x2 = float(line2[1])
                             y2 = float(line2[2])
-
-                            #print (x1, end = ", ")
-                            #print (y1)
-                            #print (x2, end = ", ")
-                            #print (y2)
-
+    
+                    
                             Dist += float(distance(x1, y1, x2, y2))
-
-                            #print (float(distance(x1, y1, x2, y2)), end = ", ")
-
+    
+                    
                             if foundFirstTime == False:
                                 line1 = getInfo(current_line)
                                 line2 = getInfo(prev_line)
-
+    
                                 if (checkFirstMove(line1, line2)):
                                     Time_First = line1[0]
                                     foundFirstTime = True
-
+    
                         #To prepare for trial #24
                         if TrialNo == 24:
                             curr_line = current_line
-
+    
                         prev_line = current_line
 
-                #Does trial #24
+                    #Does trial #24
                 info = getInfo(prev_line)
                 Time_Elapsed = float(info[0])
                 Status = checkFail(Time_Elapsed)
                 foundFirstTime = False
                 prev_line = ""
                 input_line = appendInput()
-
-                col = 0
+    
+                col = 0 
 
                 for i in input_line:
                     sheet.write(row, col, i)
                     col += 1
 
-                #print(input_line)
+                            #print(input_line)
 
     wb.close()
